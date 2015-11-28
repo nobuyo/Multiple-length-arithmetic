5d263feae862a8885c482023a47b905b4f4cd7c1
nobuyo <world_fantasy_2017@live.jp>
2015-11-28 10:52:24 +0900

    add source file

---
+++ main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include "multiple-length-arithmetic.h"
+
+int compare2Num(int a, int b)
+{
+	if (a > b) {
+		return 1;
+	}
+	else if (a < b) {
+		return -1;
+	}
+	else if (a == b) {
+		return 0;
+	}
+}
+
+void compTest(NUMBER *a, NUMBER *b)
+{
+	int i;
+	int x, y;
+	int resultOpe, resultCmp;
+	x = random();
+	y = random();
+
+	setInt(a, x);
+	setInt(b, y);
+
+	for (i=0; i<10000; i++) {
+		resultOpe = compare2Num(x,y);
+		resultCmp = numComp(a,b);
+		if(resultCmp == resultOpe)
+			printf("OK\n");
+		else
+			printf("NG\n");
+	}
+}
+
+
+int main(void)
+{
+	int i;
+	int test;
+	NUMBER b,c,d;
+	srandom(time(NULL));
+
+
+	for (i=0; i<KETA; i++) {
+		b.n[i] = i;
+		c.n[i] = i;
+		d.n[i] = i;
+	}
+
+	b.sign = 1;
+	c.sign = 0;
+	d.sign = 1;
+
+	clearByZero(&b);
+
+	b.n[0] = 5;
+	b.n[1] = 3;
+
+	dispNumber(&b);
+	putchar('\n');
+
+	dispNumberZeroSuppress(&b);
+	putchar('\n');
+
+	sedRnd(&b,8);
+	dispNumber(&b);
+	putchar('\n');
+
+	printf("Zero Check => %d\n", isZero(&b));
+
+	clearByZero(&c);
+	dispNumber(&c);
+	putchar('\n');
+
+	getAbs(&b,&c);
+	dispNumber(&c);
+	putchar('\n');
+
+	sedRnd(&b,8);
+	sedRnd(&c,8);
+	dispNumber(&b);
+	putchar('\n');
+	dispNumber(&c);
+	putchar('\n');
+	swap(&b,&c);
+	dispNumber(&b);
+	putchar('\n');
+	dispNumber(&c);
+	putchar('\n');
+
+	test = random();
+	printf("%d\n", test);
+	setInt(&b,test);
+	dispNumber(&b);
+	putchar('\n');
+
+	compTest(&b,&c);
+
+	return 0;
+}
---
+++ multiple-length-arithmetic.h
@@ -0,0 +1,243 @@
+#ifndef MULTIPLE_LENGTH_ARITHMETIC
+#define MULTIPLE_LENGTH_ARITHMETIC
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define KETA 15
+
+typedef struct 
+{
+	int n[KETA];
+	int sign;
+} NUMBER;
+
+void setSign(NUMBER *a, int s);
+int getSign(NUMBER *a);
+void clearByZero(NUMBER *a);
+void dispNumber(NUMBER *a);
+void dispNumberZeroSuppress(NUMBER *a);
+void sedRnd(NUMBER *a,int n);
+void copyNumber(NUMBER *a, NUMBER *b);
+void getAbs(NUMBER *a, NUMBER *b);
+int isZero(NUMBER *a);
+int mulBy10(NUMBER *a, NUMBER *b);
+int divBy10(NUMBER *a, NUMBER *b);
+void swap(NUMBER *a, NUMBER *b);
+int setInt(NUMBER *a, int x);
+int numComp(NUMBER *a, NUMBER *b);
+
+
+
+
+void setSign(NUMBER *a, int s) {
+	if (s == -1 || s == 1)
+		a->sign = s;
+}
+
+int getSign(NUMBER *a) {
+	if (a->sign == -1 || a->sign == 1)
+		return a->sign;
+}
+
+void clearByZero(NUMBER *a)
+{
+	int i;
+	for (i=0; i<KETA; i++) {
+		a->n[i] = 0;
+	}
+
+	setSign(a,1);
+}
+
+void dispNumber(NUMBER *a)
+{
+	int i;
+	if (getSign(a) == 1) {
+		printf("+");
+	}
+	else {
+		printf("-");
+	}
+	for (i=KETA-1; i>=0; i--) {
+		printf("%2d",a->n[i]);
+	}
+}
+
+void dispNumberZeroSuppress(NUMBER *a)
+{
+	int i;
+	int flag = 0;
+
+	if (getSign(a) == 1) {
+		printf("+");
+	}
+	else {
+		printf("-");
+	}
+
+	for (i=KETA-1; i>=0; i--) {
+		if (a->n[i] != 0) {
+			flag = 1;
+		}
+		if (flag) {
+			printf("%2d",a->n[i]);
+		}
+	}
+
+}
+
+void sedRnd(NUMBER *a,int n)
+{
+	int i;
+	clearByZero(a);
+
+	if (random()%100 > 50) {
+		setSign(a,1);
+	}
+	else {
+		setSign(a,-1);
+	}
+	for (i=0; i<n; i++) {
+		a->n[i] = random()%10;
+	}
+}
+
+void copyNumber(NUMBER *a, NUMBER *b)
+{
+	int i;
+	setSign(b,getSign(a));
+	for (i=0; i<KETA; i++) {
+		b->n[i] = a->n[i];
+	}
+}
+
+void getAbs(NUMBER *a, NUMBER *b)
+{
+	copyNumber(a, b);
+	setSign(b,1);
+}
+
+int isZero(NUMBER *a)
+{
+	int i;
+	if (getSign(a) == -1) {
+		return -1;
+	}
+	for (i=0; i<KETA; i++) {
+		if (a->n[i] != 0) {
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+int mulBy10(NUMBER *a, NUMBER *b)
+{
+	int i;
+	copyNumber(a,b);
+
+	for (i=KETA-1; i>0; i--) {
+		b->n[i] = b->n[i-1];
+	}
+
+	b->n[0] = 0;
+
+	if (a->n[KETA-1] != 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+int divBy10(NUMBER *a, NUMBER *b)
+{
+	int i;
+	copyNumber(a,b);
+
+	for (i=0; i<KETA; i++) {
+		b->n[i-1] = b->n[i];
+	}
+
+	b->n[KETA-1] = 0;
+
+	if (a->n[0] != 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+void swap(NUMBER *a, NUMBER *b)
+{
+	NUMBER tmp;
+	copyNumber(a, &tmp);
+	copyNumber(b, a);
+	copyNumber(&tmp, b);	
+}
+
+int setInt(NUMBER *a, int x)
+{
+	int i,y;
+	clearByZero(a);
+	y = abs(x);
+
+	if (x < 0) {
+		setSign(a, -1);
+	}
+	else {
+		setSign(a, 1);
+	}
+
+	for (i=0; i<10; i++) {
+		a->n[i] = y % 10;
+		y -= a->n[i];
+		y /= 10;
+	}
+
+	if (KETA > 10)
+		return -1;
+
+	return 0;
+}
+
+
+int numComp(NUMBER *a, NUMBER *b)
+{
+
+	int i;
+
+	if (getSign(a) == 1 && getSign(b) == -1) {
+		return 1;
+	}
+	else if (getSign(a) == -1 && getSign(b) == 1) {
+		return -1;
+	}
+	else if (getSign(a) == 1 && getSign(b) == 1) {
+
+		for (i = KETA-1; i >= 0; i--)
+		{
+			if (a->n[i] > b->n[i])
+				return 1;
+			else if (a->n[i] < b->n[i])
+				return -1;
+		}
+		return 0;
+	}
+	else if (getSign(a) == -1 && getSign(b) == -1) {
+
+		for (i = KETA-1; i >= 0; i--)
+		{
+			if (a->n[i] > b->n[i])
+				return -1;
+			else if (a->n[i] < b->n[i])
+				return 1;
+		}
+		return 0;
+	}
+}
+
+#endif
